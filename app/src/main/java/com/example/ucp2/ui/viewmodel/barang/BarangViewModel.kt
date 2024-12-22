a6898e6b0a327ca41582548b5de3c5d7f85de000
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-22 23:14:38 +0700

    Membuat class FormErrorState untuk menyimpan kesalahan form, dengan fungsi isValid untuk memeriksa apakah tidak ada kesalahan

--- app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BarangViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.ucp2.ui.viewmodel.barang
 
-
+data class FormErrorState(
+    val id: String? = null,
+    val nama: String? = null,
+    val deskripsi: String? = null,
+    val harga: String? = null,
+    val stok: String? = null,
+    val namaSuplier: String? = null,
+){
+    fun isValid(): Boolean{
+        return id == null && nama == null && deskripsi == null &&
+                harga == null  && stok == null && namaSuplier == null
+    }
+}
 
 fun BarangEvent.toBarangEntity(): Barang = Barang(
     id = id,
