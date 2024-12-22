3d28fc93e48552d485b51cd9052471356ca4692c
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-22 23:25:05 +0700

    Membuat kelas BarangViewModel untuk mengelola status UI barang, termasuk memperbarui state berdasarkan input pengguna dan memvalidasi data yang dimasukkan

--- app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BarangViewModel.kt
@@ -1,6 +1,42 @@
 package com.example.ucp2.ui.viewmodel.barang
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.repository.RepositoryBarang
+import kotlinx.coroutines.launch
+
+class BarangViewModel(private val repositoryBarang: RepositoryBarang
+): ViewModel() {
+
+    var uiStateBrg by mutableStateOf(BrgUIState())
+
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(barangEvent: BarangEvent) {
+        uiStateBrg = uiStateBrg.copy(
+            barangEvent = barangEvent,
+        )
+    }
+
+    // Validasi data import pengguna
+    private fun validateFields(): Boolean {
+        val event = uiStateBrg.barangEvent
+        val errorState = FormErrorState(
+            id = if (event.id.isNotEmpty()) null else "Id tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            deskripsi = if (event.deskripsi.isNotEmpty()) null else "Deskripsi tidak boleh kosong",
+            harga = if (event.harga.isNotEmpty()) null else "Harga tidak boleh kosong",
+            stok = if (event.stok.isNotEmpty()) null else "Stok Tidak Boleh Kosong",
+            namaSuplier = if (event.namaSuplier.isNotEmpty()) null else "Nama Suplier tidak boleh kosong",
+        )
+        uiStateBrg = uiStateBrg.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+}
 
 
 data class BrgUIState(
