d04e6361594cfc7ecaae73c6745f28c0d32a49da
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-22 22:47:20 +0700

    Mendefiniskan data class barang dengan atribut id, nama, deskripsi, harga, stok, dan namaSuplier sebagai entitas untuk tabel Barang

---
+++ app/src/main/java/com/example/ucp2/data/entity/Barang.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Barang(
+    @PrimaryKey
+    val id: String,
+    val nama: String,
+    val deskripsi: String,
+    val harga: String,
+    val stok: Int,
+    val namaSuplier: String,
+)
+
