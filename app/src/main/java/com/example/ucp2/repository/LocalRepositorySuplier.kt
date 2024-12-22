c977d733885c2bd2ef209a46569da7408c3a0b06
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-22 23:46:02 +0700

    Membuat kelas LocalRepositorySuplier yang mengimplementasikan RepositorySuplier dengan menggunakan suplierDao untuk akses data lokal

---
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositorySuplier.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.dao.SuplierDao
+import com.example.ucp2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositorySuplier (
+    private val suplierDao: SuplierDao
+) : RepositorySuplier{
+
+
+}
