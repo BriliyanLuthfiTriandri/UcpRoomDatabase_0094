6743194c1795363a9e914d2a6013b866dceb6a69
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-22 22:52:55 +0700

    Membuat fungsi TopAppBar dengan parameter untuk tombol kembali, menampilkan judul, dan modifier untuk penyesuaian dengan tampilan

---
+++ app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
@@ -0,0 +1,28 @@
+package com.example.ucp2.ui.customwidget
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    modifier: Modifier = Modifier
+) {
+
+}
+
