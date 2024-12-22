427841edad30787639b9078800ac654885de9098
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-22 23:04:44 +0700

    Mendefinisikan data class BarangEvent dengan atribut id, nama, deskripsi, harga, stok, dan namaSuplier, yang semuanya memiliki nilai default kosong.

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/BarangViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2.ui.viewmodel.barang
+
+
+
+data class BarangEvent(
+    val id : String = "",
+    val nama : String = "",
+    val deskripsi : String = "",
+    val harga : String = "",
+    val stok : String = "",
+    val namaSuplier : String = "",
+)
