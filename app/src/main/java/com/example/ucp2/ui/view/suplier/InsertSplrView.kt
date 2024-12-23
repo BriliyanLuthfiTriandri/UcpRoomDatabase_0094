e6d746fb3d1ca1ecafeb2a6b55c1c40c0284362d
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-23 16:24:40 +0700

    Menambahkan formulir untuk input data supplier, termasuk ID, Nama, Kontak, dan Alamat, dengan validasi error

---
+++ app/src/main/java/com/example/ucp2/ui/view/suplier/InsertSplrView.kt
@@ -0,0 +1,115 @@
+package com.example.ucp2.ui.view.suplier
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.customwidget.TopAppBar
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
+import com.example.ucp2.ui.viewmodel.suplier.FormErrorState
+import com.example.ucp2.ui.viewmodel.suplier.SplrUIState
+import com.example.ucp2.ui.viewmodel.suplier.SuplierEvent
+import com.example.ucp2.ui.viewmodel.suplier.SuplierViewModel
+import kotlinx.coroutines.launch
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun FormSuplier(
+    suplierEvent: SuplierEvent = SuplierEvent(),
+    onValueChange: (SuplierEvent) -> Unit =  {},
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+){
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ){
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.id ,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(id = it))
+            },
+            label = { Text("ID") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            isError = errorState.id != null,
+            placeholder = { Text("Masukan ID") },
+        )
+        Text(
+            text = errorState.id ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.nama,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukan Nama") },
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.kontak,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(kontak = it))
+            },
+            label = { Text("Kontak") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            isError = errorState.kontak != null,
+            placeholder = { Text("Masukan Nomor Anda") },
+        )
+        Text(
+            text = errorState.kontak ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.alamat,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(alamat = it))
+            },
+            label = { Text("Alamat") },
+            isError = errorState.alamat != null,
+            placeholder = { Text("Masukan Alamat Anda") },
+        )
+        Text(
+            text = errorState.alamat ?: "",
+            color = Color.Red
+        )
+    }
+}
