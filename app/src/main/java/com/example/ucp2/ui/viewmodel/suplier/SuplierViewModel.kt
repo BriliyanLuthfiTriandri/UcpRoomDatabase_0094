ef20b22a4d4f1bbaec4a907bc49804117139ebbc
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-23 13:18:50 +0700

    Menambahkan SuplierEvent untuk menyimpan data detail suplier

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/suplier/SuplierViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.ucp2.ui.viewmodel.suplier
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Suplier
+import com.example.ucp2.repository.RepositorySuplier
+import com.example.ucp2.ui.viewmodel.barang.BarangEvent
+import kotlinx.coroutines.launch
+
+
+
+data class SuplierEvent(
+    val id : String = "",
+    val nama : String = "",
+    val kontak : String = "",
+    val alamat : String = ""
+)
