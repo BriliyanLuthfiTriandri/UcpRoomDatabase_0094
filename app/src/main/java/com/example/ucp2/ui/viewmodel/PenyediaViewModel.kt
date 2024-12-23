d1bb3b4d0382d07326e8f3c2d7e57fbc772d6a9a
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-23 13:49:43 +0700

    Menambahkan fungsi untuk mendapatkan instance WarungApp dari CreationExtras

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.ucp2.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.WarungApp
+import com.example.ucp2.ui.viewmodel.barang.BarangViewModel
+import com.example.ucp2.ui.viewmodel.barang.DetailBrgViewModel
+import com.example.ucp2.ui.viewmodel.barang.HomeBrgViewModel
+import com.example.ucp2.ui.viewmodel.barang.UpdateBrgViewModel
+import com.example.ucp2.ui.viewmodel.suplier.HomeSplrViewModel
+import com.example.ucp2.ui.viewmodel.suplier.SuplierViewModel
+
+
+fun CreationExtras.warungApp(): WarungApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as WarungApp)
