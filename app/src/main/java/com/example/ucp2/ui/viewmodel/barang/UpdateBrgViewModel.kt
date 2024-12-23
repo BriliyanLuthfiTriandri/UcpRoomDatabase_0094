63a0e5f0191e6b4b57d2f840cb49a457cb41ab6c
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-23 12:41:03 +0700

    menambahkan fungsi untuk mengubah Barang menjadi BrgUIState untuk tampilan detail barang

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/UpdateBrgViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.ucp2.ui.viewmodel.barang
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.repository.RepositoryBarang
+import com.example.ucp2.ui.navigation.DestinasiUpdateBrg
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+
+
+fun Barang.toUIStateBrg(): BrgUIState = BrgUIState (
+    barangEvent = this.toDetailUiEvent()
+)
