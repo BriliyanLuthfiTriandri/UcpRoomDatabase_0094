bd8471b725d1dc13f0eaa016efef9285e6c749ba
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-23 16:53:19 +0700

    Menambahkan tampilan halaman utama dengan kartu navigasi untuk barang dan suplier

---
+++ app/src/main/java/com/example/ucp2/ui/view/HomeAwal.kt
@@ -0,0 +1,281 @@
+package com.example.ucp2.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ucp2.R
+
+@Preview(showBackground = true)
+@Composable
+fun HomeAwal(
+    onAddBarang: () -> Unit = { },
+    onAddSuplier: () -> Unit = { },
+    onHomeBarang: () -> Unit = { },
+    onHomeSuplier: () -> Unit = { },
+
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Card(
+            shape = RoundedCornerShape(bottomEnd = 70.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFFF91A4)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                Color(0xFFFF91A4),
+                                Color.Cyan
+                            )
+                        )
+                    ),
+                contentAlignment = Alignment.TopStart
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 16.dp, top = 8.dp),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "Menu Icon",
+                        modifier = Modifier.size(30.dp),
+                        tint = Color.White
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "Warung Ceunah",
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text(
+                        text = "Halo, selamat menikmati !!!",
+                        fontSize = 20.sp,
+                        color = Color.White
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .size(120.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logowarung),
+                        contentDescription = "Logo",
+                        modifier = Modifier.size(150.dp),
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Card(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(150.dp)
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .clickable { onHomeBarang() }
+                        .background(
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    Color.Red,
+                                    Color.Cyan
+                                )
+                            )
+                        ),
+                    colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.barangicon),
+                                contentDescription = "Barang Icon",
+                                modifier = Modifier.size(80.dp)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Barang",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+
+                Card(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(150.dp)
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .clickable { onAddBarang() }
+                        .background(
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    Color.Magenta,
+                                    Color.Cyan
+                                )
+                            )
+                        ),
+                    colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.tambahbarang),
+                                contentDescription = "Tambah Barang Icon",
+                                modifier = Modifier.size(80.dp)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Tambah Barang",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Card(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(150.dp)
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .clickable { onHomeSuplier() }
+                        .background(
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    Color.Red,
+                                    Color.Cyan
+                                )
+                            )
+                        ),
+                    colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.supliericon),
+                                contentDescription = "Suplier Icon",
+                                modifier = Modifier.size(80.dp)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Suplier",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+
+                Card(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(150.dp)
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .clickable { onAddSuplier() }
+                        .background(
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    Color.Magenta,
+                                    Color.Cyan
+                                )
+                            )
+                        ),
+                    colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.tambahsplricon),
+                                contentDescription = "Tambah Suplier Icon",
+                                modifier = Modifier.size(80.dp)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Tambah Suplier",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
