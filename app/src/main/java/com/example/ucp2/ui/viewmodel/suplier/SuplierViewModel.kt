6a5b424f5310297b70002fc82fa363fe311af508
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-23 13:47:36 +0700

    Menambahkan SuplierViewModel untuk mengelola state UI suplier, validasi data, dan menyimpan data ke repository

--- app/src/main/java/com/example/ucp2/ui/viewmodel/suplier/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/suplier/SuplierViewModel.kt
@@ -10,7 +10,60 @@ import com.example.ucp2.repository.RepositorySuplier
 import com.example.ucp2.ui.viewmodel.barang.BarangEvent
 import kotlinx.coroutines.launch
 
+class SuplierViewModel(private val repositorySuplier: RepositorySuplier): ViewModel() {
 
+    var uiStateSplr by mutableStateOf(SplrUIState())
+
+
+    fun updateState(suplierEvent: SuplierEvent){
+        uiStateSplr = uiStateSplr.copy(
+            suplierEvent = suplierEvent,
+        )
+    }
+
+    // Validasi data import pengguna
+    private fun validateFields(): Boolean {
+        val event = uiStateSplr.suplierEvent
+        val errorState = FormErrorState(
+            id = if (event.id.isNotEmpty()) null else "Id tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            kontak = if (event.kontak.isNotEmpty()) null else "Kontak tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+        )
+        uiStateSplr = uiStateSplr.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    //Menyimpan data ke repository
+    fun saveData() {
+        val currentEvent = uiStateSplr.suplierEvent
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositorySuplier.insertSplr(currentEvent.toSuplierEntity())
+                    uiStateSplr = uiStateSplr.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        suplierEvent = SuplierEvent(), //Reset input form
+                        isEntryValid = FormErrorState() // Reset error state
+                    )
+                } catch (e: Exception) {
+                    uiStateSplr = uiStateSplr.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        } else {
+            uiStateSplr = uiStateSplr.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data anda."
+            )
+        }
+    }
+    //Reset pesan Snackbar setelah ditampilkan
+    fun resetSnackBarMessage(){
+        uiStateSplr = uiStateSplr.copy(snackBarMessage = null)
+    }
+}
 
 data class SplrUIState(
     val suplierEvent: SuplierEvent = SuplierEvent(),
