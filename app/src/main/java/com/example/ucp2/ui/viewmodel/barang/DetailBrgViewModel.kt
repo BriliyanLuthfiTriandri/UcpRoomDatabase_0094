c8fb333e88ed5738711fee70e37ed46fcf277ada
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-23 12:20:17 +0700

    menambahkan fungsi untuk mengubah Barang ke BarangEvent untuk penanganan event UI detail

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/barang/DetailBrgViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.ui.viewmodel.barang
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.repository.RepositoryBarang
+import com.example.ucp2.ui.navigation.DestinasiDetailBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+
+
+fun Barang.toDetailUiEvent () : BarangEvent{
+    return BarangEvent(
+        id = id,
+        nama = nama,
+        deskripsi = deskripsi,
+        harga = harga,
+        stok = stok.toString(),
+        namaSuplier = namaSuplier
+    )
+}
